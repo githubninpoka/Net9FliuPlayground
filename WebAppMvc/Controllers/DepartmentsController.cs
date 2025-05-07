4b4898a385b3d36903c73a329b422418e9ba7214
Ninpoka <github.com@ninpoka.nl>
2025-05-07 14:35:39 +0200

    with apicontroller attribute, endpoint throws error immediately on modelerrors instead of processing it. Good for API's, maybe less so for MVC Gui.

--- WebAppMvc/Controllers/DepartmentsController.cs
+++ WebAppMvc/Controllers/DepartmentsController.cs
@@ -3,7 +3,13 @@ using WebAppMvc.Models;
 
 namespace WebAppMvc.Controllers;
 
-public class DepartmentsController
+[ApiController]
+[Route("/departments")]
+// just for reference, by adding this attribute, the controller starts behaving differently.
+// instead of processing a request with model errors, the api will immediately short circuit and throw an error
+// for this to work, the endpoint itself must also have a route defined. (!)
+
+public class DepartmentsController : Controller
 {
     public string Index()
     {
@@ -17,8 +23,19 @@ public class DepartmentsController
 
     [HttpPost]
     [Consumes("application/xml")]
+    [Route("create")]
     public object Create([FromBody]Department department)
     {
+        ModelState.AddModelError("TestKey","TestErrorValue");
+        foreach(var value in ModelState.Values)
+        {
+            foreach (var error in value.Errors)
+            {
+                Console.WriteLine($"One error: {error.ErrorMessage}");
+            }
+        }
+        // we can do something with the modelstate here. but probably somewhere in the filterpipeline there is something better...
+
         return department;
     }
 
