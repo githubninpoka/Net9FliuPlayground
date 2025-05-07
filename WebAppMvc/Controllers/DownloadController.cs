6ecc4426dfb4a86c5a6a475263faf4ce9f1e8d7f
Ninpoka <github.com@ninpoka.nl>
2025-05-07 21:48:16 +0200

    added a controller for displaying various methods of downloading files.

---
+++ WebAppMvc/Controllers/DownloadController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMvc.Controllers;
+
+public class DownloadController : Controller
+{
+    public VirtualFileResult Vf()
+    {
+        return new VirtualFileResult("JustADownloadableFile.txt", "text/plain");
+    }
+    public PhysicalFileResult Pf()
+    {
+        return new PhysicalFileResult($"{Environment.CurrentDirectory}\\PhysicalFiles\\JustADownloadableFileToo.txt", "text/plain");
+    }
+    public FileContentResult Fc()
+    {
+        byte[] file = System.IO.File.ReadAllBytes($"{Environment.CurrentDirectory}\\PhysicalFiles\\broeikas.jpg");
+        return new FileContentResult(file,"application/jpg");
+    }
+}
