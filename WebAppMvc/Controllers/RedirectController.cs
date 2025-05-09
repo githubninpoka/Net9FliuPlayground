39178d83550bd9017e12bceb8b56f12b057566a2
Ninpoka <github.com@ninpoka.nl>
2025-05-09 08:49:07 +0200

    Sample various way to redirect

---
+++ WebAppMvc/Controllers/RedirectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMvc.Controllers;
+
+public class RedirectController : Controller
+{
+    public IActionResult Index()
+    {
+        //return Redirect("https://duckduckgo.com");
+        //return new RedirectResult("https://duckduckgo.com");
+        //return new RedirectToActionResult("Index", "Home",null);
+        //return RedirectToAction("Index", "Home");
+        return LocalRedirect("/Home/Index");
+    }
+}
